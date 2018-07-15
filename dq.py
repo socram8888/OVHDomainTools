d587bf1803e2648638bb03c4e9495d28ab4a23e9
Marcos Del Sol Vives <marcos@dracon.es>
2018-07-15 23:02:51 +0200

    Added sorting

--- dq.py
+++ dq.py
@@ -13,7 +13,9 @@ import traceback
 class Sorting(Enum):
 	NONE = 0
 	PRICE = 1
-	ALPHABETIC = 2
+	RENEW = 2
+	ORDER = 3
+	ALPHABETIC = 4
 
 class DomainInfo:
 	def __init__(self, name, order, renew):
@@ -23,8 +25,31 @@ class DomainInfo:
 
 class DomainCmd(Cmd):
 	prompt = '(dq) '
-	TRUES = ['yes', 'true', 'enabled', 'on']
-	FALSES = ['no', 'false', 'disabled', 'off']
+	BOOLEANS = {
+			'yes': True,
+			'enabled': True,
+			'true': True,
+			'on': True,
+
+			'no': False,
+			'disabled': False,
+			'false': False,
+			'off': False
+	}
+	SORTING_NAMES = {
+			'price': Sorting.PRICE,
+			'renew': Sorting.RENEW,
+			'order': Sorting.ORDER,
+			'alphabetic': Sorting.ALPHABETIC,
+			'no': Sorting.NONE,
+			'disabled': Sorting.NONE,
+			'false': Sorting.NONE,
+			'off': Sorting.NONE
+	}
+	SORTING_DIRECTION = {
+			'ascending': True,
+			'descending': False
+	}
 	RESETLINE = "\x1b[1K\r"
 
 	def __init__(self):
@@ -38,7 +63,7 @@ class DomainCmd(Cmd):
 
 		# Sorting
 		self.sorting = Sorting.NONE
-		self.sort_ascendent = True
+		self.sort_ascendending = True
 
 		# Status querying
 		self.cart_id = None
@@ -61,10 +86,13 @@ class DomainCmd(Cmd):
 		arg = arg.strip().casefold()
 
 		if arg != '':
-			if any(x for x in DomainCmd.FALSES if x.startswith(arg)):
-				value = None
+			maxlen_enabled = True
+			try:
+				maxlen_enabled = self._parse_bool(arg)
+			except:
+				pass
 
-			else:
+			if maxlen_enabled:
 				try:
 					value = int(arg)
 				except ValueError:
@@ -74,6 +102,8 @@ class DomainCmd(Cmd):
 				if value < 4:
 					print('Max length may not be less than 4', file=sys.stderr)
 					return
+			else:
+				value = None
 
 			self.max_length = value
 
@@ -106,19 +136,55 @@ class DomainCmd(Cmd):
 
 		print('Second-level domains are %s' % ('enabled' if self.include_sld else 'disabled'), file=sys.stderr)
 
+	def _partial_key_match(self, map, partial):
+		partial = partial.casefold()
+		keys = list()
+		values = set()
+
+		for key, value in map.items():
+			if key.startswith(partial):
+				keys.append(key)
+				values.add(value)
+
+		if len(values) == 0:
+			raise ValueError('Expected any of %s' % str(list(map.keys())))
+		elif len(values) > 1:
+			raise ValueError('"%s" may refer to any of %s' % (partial, str(keys)))
+
+		return next(iter(values))
+
 	def _parse_bool(self, text):
-		text = text.casefold()
-		trues = [x for x in DomainCmd.TRUES if x.startswith(text)]
-		falses = [x for x in DomainCmd.FALSES if x.startswith(text)]
+		return self._partial_key_match(DomainCmd.BOOLEANS, text)
 
-		if trues:
-			if falses:
-				raise ValueError('"%s" may refer to %s, or to %s' % (text, str(trues), str(falses)))
-			return True
-		if falses:
-			return False
+	def do_sort(self, args):
+		args = args.split()
 
-		raise ValueError("Expected any of %s, or %s" % (str(DomainCmd.TRUES), str(DomainCmd.FALSES)))
+		if len(args) > 2:
+			print('Too many arguments', file=sys.stderr)
+			return
+
+		if len(args) > 0:
+			try:
+				new_sorting = self._partial_key_match(DomainCmd.SORTING_NAMES, args[0])
+			except ValueError as e:
+				print('Cannot parse mode. %s' % str(e), file=sys.stderr)
+				return
+
+			new_ascending = True
+			if len(args) > 1:
+				try:
+					new_ascending = self._partial_key_match(DomainCmd.SORTING_DIRECTION, args[1])
+				except ValueError as e:
+					print('Cannot parse direction. %s' % str(e), file=sys.stderr)
+					return
+
+			self.sorting = new_sorting
+			self.sort_ascendending = new_ascending
+
+		if self.sorting == Sorting.NONE:
+			print('Sorting disabled', file=sys.stderr)
+		else:
+			print('Sorting by %s %s' % (self.sorting.name.lower(), 'ascending' if self.sort_ascendending else 'descending'))
 
 	def do_updatetld(self, arg):
 		self._fetch_tlds()
@@ -181,17 +247,35 @@ class DomainCmd(Cmd):
 		print('got %s' % self.cart_id, file=sys.stderr)
 
 		# Reset variables
-		self.domain_info = []
-		self.failed_domains = 0
-		
-		with ThreadPoolExecutor(max_workers=10) as executor:
-			for domain in to_check:
-				executor.submit(self._check_and_update, domain)
+		if self.sorting == Sorting.NONE:
+			self._print_domain_header()
+			with ThreadPoolExecutor(max_workers=10) as executor:
+				for domain in to_check:
+					executor.submit(self._check_and_update, domain)
+		else:
+			self.domain_info = []
+			self.failed_domains = 0
 
+			with ThreadPoolExecutor(max_workers=10) as executor:
+				for domain in to_check:
+					executor.submit(self._check_and_update_sorted, domain)
+
+			self._sort_domain_list()
+			self._print_process()
+			self._print_domain_header()
+			for info in self.domain_info:
+				self._print_domain_entry(info)
+
+	def _print_process(self, line=None):
 		print(DomainCmd.RESETLINE, file=sys.stderr, end='', flush=True)
+		if line:
+			print(line, file=sys.stderr, end='', flush=True)
+
+	def _print_domain_header(self):
 		print('domain\trenew\torder')
-		for info in self.domain_info:
-			print('%s\t%s\t%s' % (info.name, info.renew, info.order))
+
+	def _print_domain_entry(self, info):
+		print('%s\t%s\t%s' % (info.name, info.renew, info.order))
 
 	def _domain_check_list(self, domains):
 		to_check = set()
@@ -213,8 +297,20 @@ class DomainCmd(Cmd):
 		return to_check
 
 	def _check_and_update(self, domain):
+		try:
+			info = self._check_domain_status(domain)
+		except Exception as e:
+			with self.print_lock:
+				traceback.print_last()
+			info = None
+
 		with self.print_lock:
-			print(DomainCmd.RESETLINE + '%i/%i: %s' % (len(self.domain_info), self.failed_domains, domain), file=sys.stderr, end='', flush=True)
+			if info is not None:
+				self._print_domain_entry(info)
+
+	def _check_and_update_sorted(self, domain):
+		with self.print_lock:
+			self._print_process('%i/%i: %s' % (len(self.domain_info), self.failed_domains, domain))
 
 		try:
 			info = self._check_domain_status(domain)
@@ -227,7 +323,7 @@ class DomainCmd(Cmd):
 			if info is not None:
 				self.domain_info.append(info)
 			else:
-				self.failed_domains += 1			
+				self.failed_domains += 1
 
 	def _check_domain_status(self, domain):
 		params = {
@@ -258,6 +354,22 @@ class DomainCmd(Cmd):
 			return None
 
 		return DomainInfo(domain, orderprice, renewprice)
+
+	def _sort_domain_list(self):
+		if self.sorting == Sorting.PRICE:
+			func = lambda x: max(x.renew, x.order)
+		elif self.sorting == Sorting.RENEW:
+			func = lambda x: x.renew
+		elif self.sorting == Sorting.ORDER:
+			func = lambda x: x.order
+		elif self.sorting == Sorting.ALPHABETIC:
+			func = lambda x: x.name
+		else:
+			raise Exception('What the fuck %s' % str(self.sorting))
+
+		self.domain_info.sort(key=func)
+		if not self.sort_ascendending:
+			self.domain_info.reverse()
 
 	def do_exit(self, arg):
 		sys.exit(0)
